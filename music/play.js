a898dbc6b6f4a6a112d1cca5856436a54c2cd8d4
Treorai <wagnerchitto@gmail.com>
2019-07-03 01:28:50 -0300

    play.js error try

--- music/play.js
+++ music/play.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 const userids = require("../tables/userids.json");
 const color = require("../tables/colortable.json");
 const url = require("../tables/urltable.json");
-const YTDL = require("ytdl-core");
-
+const YTDL = require("ytdl-core-discord");
+/*
 function Play(connection, message){
     var server = servers[message.guild.id];
     server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
@@ -39,6 +39,33 @@ module.exports.run = async (bot, message, args) => {
         message.reply("tu precisas estar em um canal pra eu cantar pra ti ^^");
     }
 }
+*/
+
+async function Play(connection, message) {
+	const server = servers[message.guild.id];
+	server.dispatcher = connection.playOpusStream(await YTDL(server.queue[0], {filter: "audioonly"}));
+	server.queue.shift();
+
+	server.dispatcher.on('end', () => {
+		if (server.queue[0]) {
+			Play(connection, message);
+		} else { connection.disconnect(); }
+	});
+}
+
+module.exports.run = async (bot, message, args) => {
+    if (!servers[message.guild.id]) servers[message.guild.id] = { queue: [] };
+
+
+	message.member.voiceChannel.join()
+		.then(connection => {
+			const server = servers[message.guild.id];
+			server.queue.push(args[0]);
+			Play(connection, message);
+        });
+
+}
+
 
 module.exports.help = {
     name: "play"
