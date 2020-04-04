586713bb8230536eb2ebb66260fd4f39143fb144
Treorai <50612887+Treorai@users.noreply.github.com>
2020-04-03 22:50:56 -0300

    Superagent Rebuild

--- commands/superagent/nander.js
+++ commands/superagent/nander.js
@@ -2,7 +2,6 @@ const Discord = require ("discord.js");
 const superagent = require("superagent");
 const color = require("../../tables/colortable.json");
 const url = require("../../tables/urltable.json");
-const fs = require("fs");
 const userids = require("../../tables/userids.json");
 
 module.exports.run = async (bot, message, args) => {
@@ -24,8 +23,10 @@ module.exports.run = async (bot, message, args) => {
 
         let birdemb = new Discord.RichEmbed()
             .setColor(color.Verdiagua)
-            .setTitle(bodytxt)
-            .setImage(bodyimg);
+            .setDescription(bodytxt)
+            .setImage(bodyimg)
+            .setTimestamp()
+            .setFooter('BOTete service(superagent)', url.BotetePP);
         message.channel.send(birdemb);
 
     } catch(error) { console.error(`${error}`); }
