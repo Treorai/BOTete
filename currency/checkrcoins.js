0549bc07ddf0280b300d1c7989fbbff9004ee6e2
Treorai <50612887+Treorai@users.noreply.github.com>
2020-04-14 01:26:06 -0300

    Update checkrcoins.js

--- currency/checkrcoins.js
+++ currency/checkrcoins.js
@@ -11,17 +11,19 @@ module.exports.run = async (bot, message, args) => {
     if(message.author.id == userids.razzor) return message.react('😑').then(message.channel.send("Não fode, André!"));
     let target = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     
+
+    let embed = new Discord.RichEmbed()
+        .setTitle(":moneybag: Recibo")
+        .setColor(color.LightGreen)
+        .setFooter("BOTete Bank(razzorcoins)", url.BOTetePP);
+
     //if(!target){
         Money.findOne({
             userID: message.author.id,
             serverID: message.guild.id
         }, (err, money) => {
             if(err) console.log(err);
-        
-            let embed = new Discord.RichEmbed()
-            .setTitle(":moneybag: Recibo")
-            .setColor(color.LightGreen)
-            .setFooter("BOTete Bank(razzorcoins)", url.BOTetePP);
+            
             if(!money){
                 embed.addField("Saldo:", "RZ$: 0", true);
                 return message.channel.send(embed);
