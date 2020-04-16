06f7b270f445147dcd516422bec39c8ed1d3565d
Treorai <50612887+Treorai@users.noreply.github.com>
2020-04-16 15:05:50 -0300

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -2,12 +2,12 @@ const { readdirSync } = require("fs");
 
 module.exports = (bot) =>{
     const load = dirs =>{
-        const commands = readdirSync(`./modules/commands/${dirs}/`).filter(d => d.endsWith('.js'));
+        const commands = readdirSync(`./modules/${dirs}/`).filter(d => d.endsWith('.js'));
         for(let file of commands){
-            const pull = require(`../modules/commands/${dirs}/${file}`);
+            const pull = require(`../modules/${dirs}/${file}`);
             bot.commands.set(pull.config.name, pull);
             if(pull.config.aliases) pull.config.aliases.forEach(a => bot.aliases.set(a, pull.config.name));
         };
     };
-    ["moderation", "rag", "services", "superagent"].forEach(x => load(x));
+    ["commands/moderation", "commands/rag", "commands/services", "commands/superagent"].forEach(x => load(x));
 };
