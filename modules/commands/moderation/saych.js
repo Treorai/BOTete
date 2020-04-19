c6b6950088e2febb91964cec5896d734dffb2b32
Treorai <50612887+Treorai@users.noreply.github.com>
2020-04-19 03:25:29 -0300

    Update saych.js

--- modules/commands/moderation/saych.js
+++ modules/commands/moderation/saych.js
@@ -5,14 +5,14 @@ const url = require("../../../tables/urltable.json");
 module.exports = {
 	config: {
 		name: "saych",
-		description: "Dá voz ao bot para uma sala recebida em args[1]",
+		description: "Dá voz ao bot para uma sala recebida em args[0]",
 		usage: ".saych <ID da sala>';;'<texto>",
 		aliases: [""]
 	},
 	run: async (bot, message, args) => {
         if(message.author.id != userids.treorai) return;
 
-        var splitarray = args.split(";;");
+        let splitarray = args.split(";;");
         if(isNaN(splitarray[0])) { return message.channel.send("Invalid Channel ID."); }
         const sendchannel = bot.channels.get(splitarray[0]);
         if(!sendchannel) { return message.channel.send("Couldn't find Channel ID.") }
