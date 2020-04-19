add85768671ba9b4463d23d19d47c9edb12d16b2
Treorai <50612887+Treorai@users.noreply.github.com>
2020-04-19 00:17:42 -0300

    upgrade from saywwc

--- modules/commands/moderation/saych.js
+++ modules/commands/moderation/saych.js
@@ -4,13 +4,14 @@ const url = require("../../../tables/urltable.json");
 
 module.exports = {
 	config: {
-		name: "saywwc",
-		description: "Confunde a dessinha.",
-		usage: ".saywwc <texto>",
+		name: "saych",
+		description: "Dá voz ao bot para uma sala recebida em args[1]",
+		usage: ".saych <ID da sala> <texto>",
 		aliases: [""]
 	},
 	run: async (bot, message, args) => {
-        const sendchannel = bot.channels.get("527281390417608726");
+        console.log(args);
+        const sendchannel = bot.channels.get("554071939271098409");
         if(message.author.id != userids.treorai) return;
         
         const sayMessage = args.join(" ");
