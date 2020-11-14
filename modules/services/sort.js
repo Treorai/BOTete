4c309bcda54b9c022c8159f7847546fc93642f2e
Treorai <50612887+Treorai@users.noreply.github.com>
2020-11-14 00:12:31 -0300

    v6.0.4

--- modules/services/sort.js
+++ modules/services/sort.js
@@ -13,7 +13,7 @@ module.exports = {
 	},
 	run: async (bot, message, args) => {
 
-		if(!args){
+		if(args === "undefined"){
 			message.channel.send(`.help sort`);
 		} else {
 			randomnumber = Math.floor(Math.random() * args.length);
