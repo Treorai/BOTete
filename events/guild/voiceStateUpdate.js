ea0f896a6827e411c123a4a125f43546bfd88290
Treorai <50612887+Treorai@users.noreply.github.com>
2020-11-13 22:50:19 -0300

    Update voiceStateUpdate.js

--- events/guild/voiceStateUpdate.js
+++ events/guild/voiceStateUpdate.js
@@ -21,43 +21,43 @@ module.exports = async (bot, oldMember, newMember) => {
   
     //weeweecrew
     if(oldMember.guild.id == idtable.guilds.weeweecrew){
-        if(oldMember.channel === null && newMember.channel !== null) {
+        if(oldMember.channelID === null && newMember.channelID !== null) {
           // User Joins a voice channel
-          bot.channels.cache.get(idtable.channels.weeweemusic).send(`${newMember.user.username} conectou-se em ${newMember.channel}.`);
-        } else if(newMember.channel === null){
+          bot.channels.cache.get(idtable.channels.weeweemusic).send(`${newMember.member.user.username} conectou-se em ${newMember.channel}.`);
+        } else if(newMember.channelID === null){
           // User leaves a voice channel
-          bot.channels.cache.get(idtable.channels.weeweemusic).send(`${oldMember.user.username} desconectou-se.`);
-        } else if(oldMember.channel !== newMember.channel){
+          bot.channels.cache.get(idtable.channels.weeweemusic).send(`${oldMember.member.user.username} desconectou-se.`);
+        } else if(oldMember.channelID !== newMember.channelID){
           // User changes voice channel
-          bot.channels.cache.get(idtable.channels.weeweemusic).send(`${newMember.user.username} moveu-se para ${newMember.channel}.`);
+          bot.channels.cache.get(idtable.channels.weeweemusic).send(`${newMember.member.user.username} moveu-se para ${newMember.channel}.`);
         };
     };
 
     //camsguild
     if(oldMember.guild.id == idtable.guilds.camsguild){
-        if(oldMember.channel === null && newMember.channel !== null) {
+        if(oldMember.channelID === null && newMember.channelID !== null) {
           // User Joins a voice channel
-          bot.channels.cache.get(idtable.channels.camsgbotch).send(`${newMember.user.username} conectou-se em ${newMember.channel}.`);
-        } else if(newMember.channel === null){
+          bot.channels.cache.get(idtable.channels.camsgbotch).send(`${newMember.member.user.username} conectou-se em ${newMember.channel}.`);
+        } else if(newMember.channelID === null){
           // User leaves a voice channel
-          bot.channels.cache.get(idtable.channels.camsgbotch).send(`${oldMember.user.username} desconectou-se.`);
-        } else if(oldMember.channel !== newMember.channel){
+          bot.channels.cache.get(idtable.channels.camsgbotch).send(`${oldMember.member.user.username} desconectou-se.`);
+        } else if(oldMember.channelID !== newMember.channelID){
           // User changes voice channel
-          bot.channels.cache.get(idtable.channels.camsgbotch).send(`${newMember.user.username} moveu-se para ${newMember.channel}.`);
+          bot.channels.cache.get(idtable.channels.camsgbotch).send(`${newMember.member.user.username} moveu-se para ${newMember.channel}.`);
         };
     };
 
     //Lenhadores
     if(oldMember.guild.id == idtable.guilds.lenhadores){
-        if(oldMember.channel === null && newMember.channel !== null) {
+        if(oldMember.channelID === null && newMember.channelID !== null) {
           // User Joins a voice channel
-          bot.channels.cache.get(idtable.channels.ldbbotch).send(`${newMember.user.username} conectou-se em ${newMember.channel}.`);
-        } else if(newMember.channel === null){
+          bot.channels.cache.get(idtable.channels.ldbbotch).send(`${newMember.member.user.username} conectou-se em ${newMember.channel}.`);
+        } else if(newMember.channelID === null){
           // User leaves a voice channel
-          bot.channels.cache.get(idtable.channels.ldbbotch).send(`${oldMember.user.username} desconectou-se.`);
-        } else if(oldMember.channel !== newMember.channel){
+          bot.channels.cache.get(idtable.channels.ldbbotch).send(`${oldMember.member.user.username} desconectou-se.`);
+        } else if(oldMember.channelID !== newMember.channelID){
           // User changes voice channel
-          bot.channels.cache.get(idtable.channels.ldbbotch).send(`${newMember.user.username} moveu-se para ${newMember.channel}.`);
+          bot.channels.cache.get(idtable.channels.ldbbotch).send(`${newMember.member.user.username} moveu-se para ${newMember.channel}.`);
         };
     };
     
